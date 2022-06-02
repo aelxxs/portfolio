4bc9afdaed8bae1e1f2189379425983e8e3dd7c3
Alexis <alexis.vielma.us@gmail.com>
2022-06-02 13:34:23 -0500

    style(server): rename render to index

--- server/src/__init__.py
+++ server/src/__init__.py
@@ -4,5 +4,5 @@ from flask import Flask
 app = Flask(__name__, static_folder='../../client/dist', static_url_path='/')
 
 @app.route('/')
-def render():
+def index():
     return app.send_static_file('index.html')
