4808263db9b5268573bbaac002d26cf3fe2d09ef
Alexis Vielma <alexis.vielma.us@gmail.com>
2022-06-22 22:42:00 -0500

    fix: docker path

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 git fetch && git reset origin/main --hard
-docker compose -f ".docker\docker-compose.yaml" down
-docker compose -f ".docker\docker-compose.yaml" up -d --build
+docker compose -f ".docker/docker-compose.yaml" down
+docker compose -f ".docker/docker-compose.yaml" up -d --build
