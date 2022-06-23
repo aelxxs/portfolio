9338237cdd126dae37b13f3dbe7506ea04e0d716
Alexis Vielma <alexis.vielma.us@gmail.com>
2022-06-22 22:17:37 -0500

    feat: add database

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,47 @@
 """
 This is the flask app
 """
-from flask import Flask, render_template
+import datetime
+from os import getenv
 from dotenv import load_dotenv
 from requests import get
+from peewee import *
+from flask import Flask, render_template, request
+from playhouse.shortcuts import model_to_dict
 
 load_dotenv()
 
 app = Flask(__name__)
+
+
+db = MySQLDatabase(
+    getenv("MYSQL_DATABASE"),
+    host=getenv("MYSQL_HOST"),
+    user=getenv("MYSQL_USER"),
+    password=getenv("MYSQL_PASSWORD"),
+)
+
+
+class TimelinePost(Model):
+    """
+    Represents a timeline post
+    """
+
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        """
+        Model metadata
+        """
+
+        database = db
+
+
+db.connect()
+db.create_tables([TimelinePost])
 
 
 @app.route("/")
@@ -16,7 +50,6 @@ def index():
     Renders the homepage.
     """
 
-    # ? I'm using some random persons API for preview reasons.
     repos = get("https://ghapi.dstn.to/aelxxs/pinned").json()["data"]
 
     return render_template("/pages/home.html", title="Alexis Vielma", repos=repos)
@@ -28,7 +61,9 @@ def projects():
     Renders the projects page.
     """
 
-    return render_template("/pages/projects.html", title="Projects")
+    timeline = TimelinePost.select().order_by(TimelinePost.created_at.desc())
+
+    return render_template("/pages/projects.html", title="Projects", timeline=timeline)
 
 
 @app.route("/thoughts")
@@ -46,3 +81,35 @@ def page_not_found(_e):
     Renders a 404 page whenever a user visits an unknown route.
     """
     return render_template("/pages/404.html", title="404"), 404
+
+
+#
+# API ENDPOINTS – WILL NOT BE MAINTAINED
+#
+
+
+@app.route("/api/timeline_post", methods=["GET"])
+def get_timeline():
+    """
+    Retrieves all timeline posts in descending order.
+    """
+
+    posts = TimelinePost.select().order_by(TimelinePost.created_at.desc())
+    timeline = [model_to_dict(post) for post in posts]
+
+    return {"timeline": timeline}
+
+
+@app.route("/api/timeline_post", methods=["POST"])
+def add_timeline():
+    """
+    Adds a new post to the timeline.
+    """
+
+    name = request.form["name"]
+    email = request.form["email"]
+    content = request.form["content"]
+
+    post = TimelinePost.create(name=name, email=email, content=content)
+
+    return model_to_dict(post)
